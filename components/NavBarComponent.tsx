2cbd32824afc1baad219c3cc87a484a6597d8ee2
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-03-11 13:54:15 +0100

    fixed a relative path

--- components/NavBarComponent.tsx
+++ components/NavBarComponent.tsx
@@ -133,7 +133,7 @@ export default function NavBarComponent() {
         <div className="flex items-center gap-5">
           {/* User Icon - Nascosta su mobile molto piccolo per pulizia */}
           <Link
-            href="api/auth/signin"
+            href="/api/auth/signin"
             className="hidden sm:block hover:text-purple-500 transition-colors duration-300"
           >
             <FontAwesomeIcon icon={faUser} />
