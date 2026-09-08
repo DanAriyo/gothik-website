fce3dceb9ec2f35cd3f8172fa0d0fab307f33136
Daniel Ariyo <oluwatobi.ariyo@studio.unibo.it>
2026-09-08 17:48:24 +0200

    finding the right height

--- components/NavBarComponent.tsx
+++ components/NavBarComponent.tsx
@@ -25,7 +25,7 @@ export default function NavBarComponent({
 
   return (
     <nav className="bg-zinc-400 border-b border-red-500 sticky top-0 z-50 text-white">
-      <div className="mx-auto px-4 h-15 flex items-center justify-between relative">
+      <div className="mx-auto px-4 h-20 flex items-center justify-between relative">
         {/* LATO SINISTRO: Logo con Sidebar Popover */}
         <div className="flex items-center">
           <NavSidebar categories={categories} />
